0228207d45ac0cc5494b074ecfe472319fa65b2e
Daniel Cunha <danielcc.jp@gmail.com>
2024-08-13 15:27:22 -0300

    feat: created selectors

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { create } from 'zustand';
 import { combine, devtools, persist } from 'zustand/middleware';
 
 import { Button } from '@/components/ui/button';
+import { createSelectors } from '@/lib/utils';
 
 interface CountAction {
   increase: () => void;
@@ -71,6 +72,12 @@ export default function Home() {
   const [middlewareCount, middlewareIncrease, middlewareReset] =
     middlewareStore((state) => [state.count, state.increase, state.reset]);
 
+  const storeSelectors = createSelectors(useStore);
+
+  const countSelector = storeSelectors.use.count();
+  const increaseSelector = storeSelectors.use.increase();
+  const resetSelector = storeSelectors.use.reset();
+
   return (
     <main className="flex min-h-screen flex-col px-24 py-16 gap-12">
       <div className="flex flex-col gap-4">
@@ -120,6 +127,21 @@ export default function Home() {
           State persists on local storage with given <br />
           name (countStore), created on first page load with initial value.
         </p>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <h1>Created Selectors (utils) (linked)</h1>
+        <div className="flex gap-4">
+          <Button onClick={increaseSelector}>Add</Button>
+          <Button variant={'outline'} onClick={resetSelector}>
+            Reset
+          </Button>
+          <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+            <code className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
+              {countSelector}
+            </code>
+          </div>
+        </div>
       </div>
     </main>
   );
